6b7cbe96c1a8ea93b8ca833610f022ced889c64e
rayssarrsilva <rayssarrsilva1@gmail.com>
2024-07-25 14:39:05 -0300

    first commit

---
+++ myproject/myproject/views.py
@@ -0,0 +1,14 @@
+#from django.http import HttpResponse
+from django.shortcuts import render 
+
+def homepage(request):
+    #return HttpResponse("Hello world, you are in the homepage")
+    return render(request, 'home.html')
+
+def about(request):
+    #return HttpResponse("u are in the about apge")
+    return render(request, 'about.html')
+
+def suporte(request):
+    return render(request, 'support.html')
+
