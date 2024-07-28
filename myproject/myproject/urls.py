44b88b8c4d7818a875e03d9b8b0fc0f367bccb6e
rayssarrsilva <rayssarrsilva1@gmail.com>
2024-07-28 13:21:49 -0300

    exclusao app teste

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -24,9 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
-    path('support/', views.suporte),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
