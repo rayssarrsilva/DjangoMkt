f5550789536cbd75b1f3ea8711cd9a89d0b13dea
rayssarrsilva <rayssarrsilva1@gmail.com>
2024-09-15 09:14:28 -0300

    verificação de login para acesso a paginas (views, layout, url)

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -7,6 +8,7 @@ def posts_list(request):
     posts = Post.objects.all().order_by('date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
+@login_required(login_url="/users/login/") #Verifica se o usuario esta logado, se nao estiver redireciona para a url inserida
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
