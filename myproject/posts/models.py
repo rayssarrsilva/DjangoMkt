2de8d1c751b867852789f082b66d05dcdca3a5a9
rayssarrsilva <rayssarrsilva1@gmail.com>
2024-09-16 17:32:48 -0300

    criação de autor no models + valida e salva o post pelo usuario especifico (views, models, html)

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User #importa o usuario 
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Post(models.Model):
     slug = models.SlugField()
     date = models.DateField(auto_now_add=True)
     banner = models.ImageField(default='fallback.png', blank=True)
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None) #relação 1 pra muitos/ on_delete: especificou que caso se o user for deletado, todos os posts dele será deletado também vice-versa
+    
     def __str__(self):
         return self.title
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from . import forms #Chapter12: importa o arquivo forms presente na pasta local
@@ -15,5 +15,14 @@ def post_page(request, slug):
 
 @login_required(login_url="/users/login/") #Verifica se o usuario esta logado, se nao estiver redireciona para a url inserida
 def post_new(request):
-    form = forms.CreatePost() #variavel para ser chamada por 'form': form
+    if request.method == 'POST': 
+        form = forms.CreatePost(request.POST, request.FILES) #request.POST: guarda todas os dados enviados pelo cliente no formulário e request.FILES: contém os arquivos enviados pelo formulário, como imagens, PDFs ou outros tipos de arquivo
+        if form.is_valid():
+            # save with user
+            newpost = form.save(commit=False) # cria uma variavel que salva o formulario
+            newpost.author = request.user 
+            newpost.save() #salva o post + autor que postou 
+            return redirect('posts:list')
+    else: #se for apenas acesso, ele cria o formulario
+        form = forms.CreatePost() #variavel para ser chamada por 'form': form// cria o formulario 
     return render(request, 'posts/post_new.html', {'form': form} )
